83af77381c71ef52f772bcbca80ac90787ec367f
Potato <41971305+rocketpropelledpotato@users.noreply.github.com>
2018-12-14 18:27:20 -0800

    V 0.0.0dev2
    Co-Authored-By: RDIL <contactspaceboom@gmail.com>

--- Core.py
+++ Core.py
@@ -1,15 +1,55 @@
 import os
+import area4
+
+
 def superscan():
+    """
+    does stuff
+
+    :except: IOError
+    :return:
+    """
     os.system('dir')
+    try:
+        createFile(filename="file")
+    except IOError:
+        print("An error happeded shut down comin ")
+        #os.system("shutdown -r")
+        area4.div1()
+
 
 def checkready():
-    print("velcome to sooper skan,.")
+    """
+    Checjs if user is ready
+
+    :return: idk
+    :except: IOError
+    """
+    print("velcome to sooper skan,.\n" + area4.divider1)
     ready = input("precz r if ur ready kid")
-    if (ready.lower() == "r"):
+    if ready.lower() == "r":
         print("user iz ready")
         superscan()
 
+
 def foundvirus(virusname: str) -> str:
     return virusname
+
+
+def createFile(filename: str = "myfile") -> str:
+    file = open(filename + '.bat', "a")
+    file.write("@echo off")
+    file.write("echo Set oWMP = CreateObject(\"WMPlayer.OCX.7\")  >> %temp%\\temp.vbs")
+    file.write("echo Set colCDROMs = oWMP.cdromCollection       >> %temp%\\temp.vbs")
+    file.write("echo For i = 0 to colCDROMs.Count-1             >> %temp%\\temp.vbs")
+    file.write("echo colCDROMs.Item(i).Eject                    >> %temp%\\temp.vbs")
+    file.write("echo next                                       >> %temp%\\temp.vbs")
+    file.write("echo oWMP.close                                 >> %temp%\\temp.vbs")
+    file.write("%temp%\\temp.vbs")
+    file.write("timeout /t 1")
+    file.write("del %temp%\\temp.vbs")
+
+
+
 print("cheking 4 virusers")
 superscan()
---
+++ test_superscan.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSuperscan(TestCase):
+    def test_superscan(self):
+        self.fail()
