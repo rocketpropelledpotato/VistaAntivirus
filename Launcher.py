298817b256976fe859d149f2d90904c5e7ebab56
Potato <41971305+rocketpropelledpotato@users.noreply.github.com>
2019-01-20 16:28:43 -0800

    bye tox

--- Launcher.py
+++ Launcher.py
@@ -4,6 +4,9 @@ launcher
 from Core import checkready
 import os
 
-theOS = os.getenv("TRAVIS_OS_NAME")
+try:
+    theOS = os.getenv("TRAVIS_OS_NAME")
+except:
+    print("ok")
 if not theOS == "linux":
     checkready()
